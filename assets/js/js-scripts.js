64df107ff3426b75a0941d036ce25e5928a410b7
Aeropink130 <antoniosalcidoav@gmail.com>
2023-09-10 17:34:28 -0600

    Modifiqué links en la barra de navegación, agregué algunas imágenes, modifiqué formularios y agregué estilos de bootstrap a todos las páginas

--- assets/js/js-scripts.js
+++ assets/js/js-scripts.js
@@ -2,7 +2,7 @@
 window.addEventListener('scroll', function () {
     if (window.scrollY > 0) {
         // La página ya no está en la parte superior, aplica los estilos aquí
-        document.body.style.backgroundColor = 'lightgray'; // Cambia el fondo
+        document.body.style.backgroundColor = 'white'; // Cambia el fondo
     } else {
         // La página está en la parte superior, puedes eliminar o restaurar estilos aquí
         document.body.style.backgroundColor = 'white'; // Restaura el fondo a su estado original
